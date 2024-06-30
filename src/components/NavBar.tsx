31c1aa5d5e7ebd28a2e42c8a8fba35c39c395526
Jose Sanchez <engineer@copydataai.com>
2024-06-30 23:41:31 +0100

    feat(components): add Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+"use server";
+import { getServerAuthSession } from "~/server/auth";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+
+export async function NavBar() {
+  const session = await getServerAuthSession();
+
+  const isSignedIn = session;
+
+  return (
+    <Navbar isBordered>
+      <NavbarBrand>
+        <Link href="/" className="font-bold text-inherit">
+          Makers
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Button as={Link} color="primary" href="#" variant="flat" asChild>
+            <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+              {session ? "Sign out" : "Sign in"}
+            </Link>
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
