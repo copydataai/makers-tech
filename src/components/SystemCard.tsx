7f2e9a1130696927afb50fe30cd11287b0c8763a
Jose Sanchez <engineer@copydataai.com>
2024-07-01 12:27:37 +0100

    feat(components): add SystemCard

---
+++ src/components/SystemCard.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Card, CardBody, Image, Spinner } from "@nextui-org/react";
+import { api } from "~/trpc/react";
+import { Bot } from "lucide-react";
+
+export function SystemCard(props: { message: string; setIsTyping: any }) {
+  const { message, setIsTyping } = props;
+  console.log(message);
+  const { isLoading, isError, error, data } =
+    api.product.questionPrompt.useQuery({ prompt: message });
+
+  if (isLoading) {
+    return <Spinner color="primary" size="sm" />;
+  }
+  console.log(data);
+
+  setIsTyping(false);
+
+  return (
+    <Card className="mr-3 bg-slate-800">
+      <CardBody className="flex items-center justify-center gap-2">
+        <Bot size={24} />
+        {data}
+      </CardBody>
+    </Card>
+  );
+}
